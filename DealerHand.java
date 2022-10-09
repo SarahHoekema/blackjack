07a183fe6b8f47c2134b969ec1d74310ccf201cc
Sarah <sarahhoekema20@gmail.com>
2022-10-09 13:49:00 -0700

    added comments

--- DealerHand.java
+++ DealerHand.java
@@ -1,12 +1,19 @@
+//Sarah Hoekema, Sean Chambers
+//CS 145
+//October 11, 2022
+//Lab 4 - Deck of Cards
+
 package blackjack;
 
+//DealerHand.java represents the hand of a Blackjack dealer
 public class DealerHand extends Hand{
-	
+	//constructor
 	public DealerHand() {
 		super();
 		super.setName("Dealer");
 	}
-	
+	//returns true if the hand value is 16 or less and the dealer must hit or
+	//false if the hand value is 17 or more and the dealer must stand
 	public boolean requestCard() {
 		if(super.getValue() <= 16) {
 			return true;
