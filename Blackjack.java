624400f0a260033511d7244901aea86297ac424b
Sean Chambers <sean.schambers@gmail.com>
2022-10-08 19:51:56 -0700

    Update Blackjack.java
    Skeletal structure of main method

--- Blackjack.java
+++ Blackjack.java
@@ -1,14 +1,49 @@
 package deckOfCards;
+import java.util.*;
 
 public class Blackjack {
+	Deck deck = new Deck();
 
 	public static void main(String[] args) {
 		introduction();
-
+		//establish initialAccount
+		do{
+			Dealer dealer = new Dealer();
+			Player player = new Player();
+			deck.shuffle();
+			//get bets
+			//deal cards
+			//get move
+			if(dealer.requestCard()){
+				do{
+					dealer.addHand(deck.dealCard());
+				}while(dealer.requestCard());
+			}
+			if(checkWin(player,dealer)){
+				//Do win logic
+			}else if(player.getCardTotal() == dealer.getCardTotal()){
+				//DO tie logic
+			}else{
+				//DO lose logic
+			}
+		}while(promptReplay() && balance != 0)
+		//print results
 	}
 	
 	public static void introduction() {
 		System.out.println("introduction here");
+	}
+
+	//returns true if player scored higher that dealer but not over 21, else returns false
+	public static boolean checkWin(Player player, Dealer dealer) {
+		//sets total to -1 if player is bust, else sets to card total
+		int playerTotal = player.getCardTotal() > 22 ? player.getCardTotal() : -1;
+		int dealerTotal = dealer.getCardTotal() > 22 ? dealer.getCardTotal() : -1;
+		return playerTotal > dealerTotal ? true : false;
+	}
+
+	public static void promptReplay() {
+		//TODO ask if new game is desired
 	}
 
 }
