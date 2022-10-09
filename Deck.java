b182a9b1373facc8d05550a386613a0dd08e335a
Sarah <sarahhoekema20@gmail.com>
2022-10-09 13:36:33 -0700

    added comments

--- Deck.java
+++ Deck.java
@@ -1,5 +1,7 @@
-//DeckOfCards.java
-//DeckOfCards class represents a deck of playing cards
+//Sarah Hoekema, Sean Chambers
+//CS 145
+//October 11, 2022
+//Lab 4 - Deck of Cards
 
 package blackjack;
 
@@ -7,15 +9,14 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Stack;
 
-
+//Deck.java represents a deck of playing cards
 public class Deck{
-  //random number generator
+  //fields
   private static final SecureRandom randomNumbers = new SecureRandom();
   private static final int NUMBER_OF_CARDS = 52;
-
   private Stack<Card> deck = new Stack<Card>(); //Card references
  
-  //constructor fills deck of Cards
+  //constructor
   public Deck(){
 	  String[] faces = {"Ace", "Deuce", "Three", "Four", "Five", "Six",
       "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
@@ -27,39 +28,42 @@ public class Deck{
       }
   }
 
-  //shuffle deck of Cards with one-pass algorithm
+  //shuffles Deck by converting the Stack to an array, switching two random cards until
+  //all cards have switched indexes, and converting is back into a Stack
   public void shuffle(){
 	Card[] tempDeck = new Card[deck.size()];
 	
+	//loads temporary array
 	for(int i = 0; i < tempDeck.length; i++) {
 		tempDeck[i] = deck.pop();
 	}
 
-    //for each Card, pick another random Card (0-51) and swap them
+    //for each Card, picks another random Card (0-51) and swap them
     for(int first = 0; first < tempDeck.length; first++){
       int second = randomNumbers.nextInt(NUMBER_OF_CARDS);
 
-      //swap current Card with randomly selected Card
+      //swaps current Card with randomly selected Card
       Card temp = tempDeck[first];
       tempDeck[first] = tempDeck[second];
       tempDeck[second] = temp;
     }
     
+    //converts array to Stack
     for(int i = 0; i < tempDeck.length; i++) {
 		deck.push(tempDeck[i]);
 	}
   }
 
-  //deal one card
+  //deals one card
   public Card dealCard(){
-    //determine whether Cards remain to be dealt
+    //determines whether Cards remain to be dealt
     if(!deck.isEmpty()){
-    	return deck.pop(); //return current Card in array
+    	return deck.pop();
     } else {
       return null; //return null to indicate that all Cards were dealt
     }
   }
-  
+  //adds cards from hand back into the deck
   public void addCards(ArrayList<Card> hand) {
 	  for(int i = 0; i < hand.size(); i++) {
 		  deck.push(hand.remove(i));
