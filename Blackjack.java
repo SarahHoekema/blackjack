c9280b71408aa25a0f5b90a7d3df7db021b23d7a
Sean Chambers <sean.schambers@gmail.com>
2022-10-10 17:20:00 -0700

    Update Blackjack.java
    additional comments and handeling for blackjack

--- Blackjack.java
+++ Blackjack.java
@@ -24,7 +24,6 @@ public class Blackjack {
 		introduction();
 		scan.nextLine();
 
-
 		//accepts players name and formats it 
 		clear();
 		System.out.println("Welcome player!");
@@ -53,9 +52,12 @@ public class Blackjack {
 			dealer.addCard(deck.dealCard());
 			player.addCard(deck.dealCard());
 			
+			//blackjack is true if the starting hand size is 2 and the 
+			boolean blackjack = (player.getHand().size() == 2 && player.getValue() == 21) ? true : false;
+		
 			boolean stand = false;
-			//loops for action until user stands or busts
-			while(player.getValue()<22 && !stand){
+			//loops for action until user stands or busts, does not enter if player got a blackjack
+			while(player.getValue()<22 && !stand && !blackjack){
 				//switch performs actions based on player input
 				switch(getMove(scan, player)){
 					case "HIT":
@@ -76,8 +78,8 @@ public class Blackjack {
 				}
 		    }
 
-			//if player is not bust the dealer takes their turn
-			if(player.getValue()<22){
+			//if player is not bust the dealer takes their turn, skips if player got a blackjack
+			if(player.getValue()<22 && !blackjack){
 				//deals to dealer per preset moves
 				System.out.println(dealer);
 				if(dealer.hitOrStand()){
@@ -88,6 +90,11 @@ public class Blackjack {
 						scan.nextLine();
 						clear();
 					}while(dealer.hitOrStand());
+					if(dealer.getValue() > 21){
+						System.out.print("Dealer busts! ");
+						scan.nextLine();
+						clear();
+					}
 				}
 			} else{
 				System.out.print("Bust! ");
@@ -97,13 +104,20 @@ public class Blackjack {
 
 			//checks if player beat the dealer
 			if(checkWin(player,dealer)){
-				System.out.println("You win!");
-				balance += player.getWager();
+				System.out.print("You win! ");
+				//increases the balance by the players wager, or increases it by 1.5 times the wager if the player had a blackjack
+				balance += blackjack ? (INT) (player.getWager() * 1.5) : player.getWager();
+				scan.nextLine();
+				clear();
 			}else if(player.getValue() == dealer.getValue()){
-				System.out.println("It's a tie. Your bet is being returned");
+				System.out.print("It's a tie. Your bet is being returned ");
+				scan.nextLine();
+				clear();
 			}else{
-				System.out.println("Better luck next time...");
+				System.out.print("Better luck next time... ");
 				balance -= player.getWager();
+				scan.nextLine();
+				clear();
 			}
 			
 			//add cards back to deck
@@ -136,16 +150,26 @@ public class Blackjack {
 
 	//requests user to place their bet
 	private static int establishBet(Scanner betAmount){
-		try {
-			clear();
-			System.out.printf("You have %d left in your account.\nWhat is your bet? ", balance);
-			String bet = betAmount.nextLine();
-			return Integer.valueOf(bet);
-		} catch (Exception e) {
-			clear();
-			System.out.println("Whoops something went wrong... " + e);
-			System.out.println("Setting bet to 2");
-			return 2;
+		while(true){
+			try {
+				clear();
+				System.out.printf("You have %d left in your account.\nWhat is your bet? ", balance);
+				int bet = Integer.valueOf(betAmount.nextLine());
+				//validates bet
+				if(bet < balance){
+					return bet;
+				}else{
+					System.out.print("Bet must be less than balance. ");
+					betAmount.nextLine();
+					clear();
+					continue;
+				}
+			} catch (Exception e) {
+				clear();
+				System.out.println("Whoops wasnt an int... ");
+				System.out.println("Setting bet to 1");
+				return 1;
+			}
 		}
 	}
 	
@@ -159,7 +183,7 @@ public class Blackjack {
 		System.out.println("This game is brought to you by Sarah Hoekema and Sean Chambers");
 		System.out.println("The goal of the game is to beat the dealers score without going over 21.");
 		System.out.println("Cards are equal to their face value.");
-		System.out.print("All face cards are worth 10 points, Aces are either 11 or 1.");
+		System.out.print("All face cards are worth 10 points, Aces are either 11 or 1. ");
 	}
 
 	//returns true if player scored higher that dealer but not over 21, else returns false
@@ -170,6 +194,7 @@ public class Blackjack {
 		return playerTotal > dealerTotal ? true : false;
 	}
 
+	//asks if the player would like to hit or stand
 	private static String getMove(Scanner nextMove, PlayerHand player){
 		clear();
 		System.out.println(player.toString());
@@ -178,6 +203,7 @@ public class Blackjack {
 		return move.toUpperCase();
 	}
 
+	//asks if the user would like to play again, provides basic error handing
 	private static boolean promptReplay(Scanner playAgainScanner) {
 		clear();
 		System.out.print("Would you like to play again? ");
@@ -200,7 +226,7 @@ public class Blackjack {
 		return false;
 	}
 
-	//clears the console screen
+	//clears the console screen, to be used after user acknowledges text 
 	private static void clear(){
 		for(int i = 0; i < 20; i++){
 			System.out.println("");
